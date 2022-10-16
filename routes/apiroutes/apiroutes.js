58b0265fbf710c06efe885afabe244ad9564cb70
DiegoM <medeldiego47@yahoo.com>
2022-10-16 17:19:22 -0500

    heroku

--- routes/apiroutes/apiroutes.js
+++ routes/apiroutes/apiroutes.js
@@ -3,12 +3,12 @@ const uniqid = require('uniqid');
 const {newNote} = require('../../functions')
 let { notes } = require('../../db/db.json')
 
-router.get('/notes', (req,res)=>{
+router.get('/api/notes', (req,res)=>{
    let results = notesArray;
    res.json(results);
 });
 
-router.post('/notes', (req,res) =>{
+router.post('/api/notes', (req,res) =>{
    const note = newNote(req.body, notes)
    res.json(note);
 }
